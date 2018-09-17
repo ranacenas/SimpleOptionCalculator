5c1048544ca6d1144ccffdcddebc38b6c3f998b0
ranacenas <36704956+ranacenas@users.noreply.github.com>
2018-09-17 12:37:36 -0400

    Added disclaimer and changed description

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # SimpleStockAndOptionCalculators
-Simple calculators that allows input
+A simple calculator that can run on the terminal for ease of input to calculate options trading scenarios
+
+DISCLAIMER: THIS CALCULATOR DOES NOT PROVIDE ANY ADVICE, RECOMMENDATIONS, OR CLAIM TO PROVIDE ANY ACCURATE INFORMATION. THIS CALCULATOR IS NOT LIABLE FOR ANY LOSSES INCURRED  AND SHOULD BE USED AT YOUR OWN DISCRETION. BY DOWNLOADING AND RUNNING THIS PROGRAM, YOU AGREE TO HAVE READ THIS DISCLAIMER AND ACCEPT ITS TERMS AND CONDITIONS. 
+
+#pd = Price Difference. 
+Input the price of the option and price you want to sell the option at. This returns the profit, buying costs, and gross revenue when selling for each number of contracts set in the setting num_con
+
+#pp = Profit Predictor
+Input the current price of the long option and the number of contracts you plan to buy / already own. Returns the cost of the trade and the price to sell the option to obtain profits determined by profit_goals
+
+
--- options_trading_1.py
+++ options_trading_1.py
@@ -1,8 +1,11 @@
 
 #settings
-com_base = 9.95
-com_per = 1.00
+com_base = 9.95 #Base commission
+com_per = 1.00 #Commission per contract
+numcon_pd = [1, 2, 4, 10] #Number of contracts
 
+
+#DO NOT CHANGE
 def total_cost_of_trade(price, num_of_contracts):         #For options trading 
     cost = (price*num_of_contracts*100)+ com_base + (com_per*num_of_contracts)
     return cost                     #x = price of contract y=number of contracts
@@ -25,43 +28,47 @@ if __name__=='__main__':
 
     count = 0
     tries = 10000
-    
-    print("""pd = Price difference. PP = Profit predicter. type "exit" to go back """)
+    print("""DISCLAIMER: THIS PROGRAM DOES NOT PROVIDE ADVICE, RECOMMENDATIONS, OR CLAIM TO 
+	PROVIDE ACCURATE INFORMATION. USE AT YOUR OWN RISK. 
+	THIS PROGRAM IS NOT LIABLE OR RESPONSIBLE FOR ANY LOSSES INCURRED, 
+	LOST PROFITS OR INDIRECT, SPECIAL, CONSEQUENTIAL DAMAGES. 
+	BY USING THIS PROGRAM, YOU AGREE 
+	TO HAVE READ THIS DISCLAIMER AND ACCEPT THE RISKS INVOLVED.
+	pd = Price difference. PP = Profit predictor. type "exit" to go back. """)
     while count < tries:
-        setting = input('enter pd or pp ')
-        while setting.lower() == 'pd':
-            p1 = input('What is the price of the option? ')
-            if p1 == 'exit':
-                break
-            p2 = input('What is the price you want to get out ')
-            numcon = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-            for i in numcon:
-                price1 = total_cost_of_trade((float(p1)), (float(i)))
-                price2 = return_cost((float(p2)), (float(i)))
-                print("%0.1f" %(i), " contract. Profit", "%.2f" %(price2 - price1), " buy cost: ", price1, "Sell Cost: ", "%.2f" %(price2))
+	    setting = input('enter pd or pp ')
+	    while setting.lower() == 'pd':
+		    p1 = input('What is the price of the option? ')
+		    if p1 == 'exit':
+			    break
+		    p2 = input('What is the price you want to get out ')
+		    for i in numcon_pd:
+			    price1 = total_cost_of_trade((float(p1)), (float(i)))
+			    price2 = return_cost((float(p2)), (float(i)))
+			    print("%0.1f" %(i), " contract(s). Profit $", "%.2f" %(price2 - price1), " buy cost: $", "%.2f" %(price1), "Sell Cost: $", "%.2f" %(price2))
 
-            
-        while setting.lower() == 'pp':
-            
-            potential = input\
-        ('What is the price of the trade ')
-            if potential == 'exit':
-                break
-            des = input('How much money do you want to make? ')
-            numcon = input('How many contracts?')
-            profit_goals = [25, 50, 75, 100]
+			
+	    while setting.lower() == 'pp':
+			
+		    potential = input\
+		('What is the price of the trade ')
+		    if potential == 'exit':
+			    break
+		    des = input('How much money do you want to make? ')
+		    numcon = input('How many contracts?')
+		    profit_goals = [25, 50, 75, 100]
 
-            p = total_cost_of_trade((float(potential)), (float(numcon)))
-            beven = (float(p)+ contractcom(float(numcon)))/(float(numcon)*100)
-            desi = (float(p) + float(des) + contractcom(float(numcon)))/ (float(numcon)* 100)
+		    p = total_cost_of_trade((float(potential)), (float(numcon)))
+		    beven = (float(p)+ contractcom(float(numcon)))/(float(numcon)*100)
+		    desi = (float(p) + float(des) + contractcom(float(numcon)))/ (float(numcon)* 100)
 
-            print ("The cost of this trade is",(p))
-            print ("To break even, sell at this price", "%.3f" %(beven))
-            print ("Sell at", "%.2f" % (desi), "to make", (des))
+		    print ("The cost of this trade is $",(p))
+		    print ("To break even, sell at this price $", "%.3f" %(beven))
+		    print ("Sell at $", "%.2f" % (desi), "to make $", (des))
 
-            for i in profit_goals:
-                scenario_profit = (float(p)+ float(i) + contractcom(float(numcon)))/ (float(numcon)* 100)
-                print("Sell at", "%.2f" % (scenario_profit), "to make", i)
+		    for i in profit_goals:
+			    scenario_profit = (float(p)+ float(i) + contractcom(float(numcon)))/ (float(numcon)* 100)
+			    print("Sell at $", "%.2f" % (scenario_profit), "to make $", i)
             
             
 
